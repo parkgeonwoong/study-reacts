a3bf0a88d37d758c50d973e3fdd6da058497b4cb
Woong <kweyz@daum.net>
2021-07-01 16:04:19 +0900

    logout

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()  // function을 이용해 app을 만듬
 const port = 5000   
 
 const bodyParser = require('body-parser'); // bodyParser 모듈 가져옴 
-const cookieParser = require('cookie-parser'); // cookie-parser 모듈
+const cookieParser = require('cookie-parser'); // co-parser 모듈
 const config = require('./config/key'); // 비밀 설정 정보
 const { auth } = require('./middleware/auth'); //auth 
 const { User } = require("./models/User"); // User.js에서 데이터 가져옴
@@ -114,6 +114,18 @@ app.get('/api/users/auth', auth, (req, res) => {
 
 })
 
+// 로그아웃 라우터
+app.get('/api/users/logout', auth, (req, res) => {
+  
+  User.findOneAndUpdate({ _id: req.user._id }, 
+    {token: "" }
+    , (err, user) => {
+      if(err) return res.json({ success: false, err });
+      return res.status(200).send({
+        success: true
+      })
+    } )
+}) 
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
