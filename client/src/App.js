0e13ff8352226a19d0376a1293748da65d0efcb0
Woong <kweyz@daum.net>
2021-07-02 15:27:17 +0900

    React Component

---
+++ client/src/App.js
@@ -0,0 +1,43 @@
+import logo from './logo.svg';
+import './App.css';
+// react-router-dom 임포트 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+// 페이지들 import 
+import LandingPage from './components/views/LandingPage/LandingPage';
+import LoginPage from './components/views/LoginPage/LoginPage';
+import RegisterPage from './components/views/RegisterPage/RegisterPage';
+
+
+function App() {
+  return (
+    <Router>
+      <div>
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+
+          <Route exact path="/login" component={LoginPage} />
+
+          <Route path="/register">
+            <RegisterPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
---
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandingPage() {
+    return (
+        <div>
+            LandingPage hahah
+        </div>
+    )
+}
+
+export default LandingPage
