2aad81d0814b5552b01bb4e0d3b6ad7760c078a0
unknown <kweyz@daum.net>
2022-01-16 19:57:08 +0900

    Class Component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,11 @@
 import "./App.css";
+import { Component } from "react";
 
-function formatDate(date) {
-  return date.toLocaleDateString();
-}
-
-function Avater(props) {
-  return (
-    <img className="Avatar" src={props.user.avatarUrl} alt={props.user.name} />
-  );
-}
-
-function UserInfo(props) {
-  return (
-    <div className="UserInfo">
-      <Avater user={props.user} />
-      <div className="UserInfo-name">{props.user.name}</div>
-    </div>
-  );
-}
-
-function App() {
-  const comment = {
-    date: new Date(),
-    text: "I hope you enjoy learning React!",
-    author: {
-      name: "Hello Kitty",
-      avatarUrl: "https://placekitten.com/g/64/64",
-    },
-  };
-  return (
-    <div className="Comment">
-      <UserInfo user={comment.author} />
-      <div className="Comment-text">{comment.text}</div>
-      <div className="Comment-date">{formatDate(comment.date)}</div>
-    </div>
-  );
+class App extends Component {
+  render() {
+    const name = "React";
+    return <div className="react">{name}</div>;
+  }
 }
 
 export default App;
