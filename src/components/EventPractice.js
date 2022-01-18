c30ab066538456c7ae7d8f1b6e3abcbf1e3eb6d5
unknown <kweyz@daum.net>
2022-01-18 22:44:41 +0900

    EventHandler state-input verification

--- src/components/EventPractice.js
+++ src/components/EventPractice.js
@@ -1,6 +1,9 @@
 import { Component } from "react";
 
 class EventPractice extends Component {
+  state = {
+    message: "",
+  };
   render() {
     return (
       <div>
@@ -9,10 +12,21 @@ class EventPractice extends Component {
           type="text"
           name="message"
           placeholder="아무거나 입력"
+          value={this.state.message}
           onChange={(e) => {
-            console.log(e.target.value);
+            this.setState({
+              message: e.target.value,
+            });
           }}
         ></input>
+        <button
+          onClick={() => {
+            alert(this.state.message);
+            this.setState({ message: "" });
+          }}
+        >
+          Check
+        </button>
       </div>
     );
   }
