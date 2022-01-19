ac273e42a2e9baa17ffa5f94f8e30fac02096738
unknown <kweyz@daum.net>
2022-01-19 14:03:14 +0900

    event: many input

--- src/components/EventPractice.js
+++ src/components/EventPractice.js
@@ -2,31 +2,40 @@ import { Component } from "react";
 
 class EventPractice extends Component {
   state = {
+    username: "",
     message: "",
   };
 
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleClick = this.handleClick.bind(this);
-  }
+  // constructor(props) {
+  //   super(props);
+  //   this.handleChange = this.handleChange.bind(this);
+  //   this.handleClick = this.handleClick.bind(this);
+  // }
 
-  handleChange(e) {
+  handleChange = (e) => {
     this.setState({
-      message: e.target.value,
+      [e.target.name]: e.target.value,
     });
-  }
+  };
 
-  handleClick() {
+  handleClick = () => {
+    alert(this.state.username + ": " + this.state.message);
     this.setState({
       message: "",
     });
-  }
+  };
 
   render() {
     return (
       <div>
         <h1>이벤트 연습</h1>
+        <input
+          type="text"
+          name="username"
+          placeholder="사용자명"
+          value={this.state.username}
+          onChange={this.handleChange}
+        ></input>
         <input
           type="text"
           name="message"
