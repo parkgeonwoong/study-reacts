86b328e6ee05f169a3caf35bc0fbf811b5427081
unknown <kweyz@daum.net>
2022-01-21 16:26:14 +0900

    LifeCycle component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import { Component } from "react";
 import "./App.css";
-import IterationSample from "./components/IterationSample";
-import ScrollBox from "./components/ScrollBox";
+import LifeCycleSample from "./components/LifeCycleSample";
+
+function getRandomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: "#000000",
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
-    return <IterationSample />;
+    return (
+      <div>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <LifeCycleSample color={this.state.color} />;
+      </div>
+    );
   }
 }
 
