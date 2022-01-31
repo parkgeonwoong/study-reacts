c9f6c9e2a45be5ec9e042ddcc07bfc45efdfb218
unknown <kweyz@daum.net>
2022-01-31 22:24:06 +0900

    styled-component mediaQuery

--- src/styles/StyledComponent.js
+++ src/styles/StyledComponent.js
@@ -6,6 +6,14 @@ const Box = styled.div`
   background: ${(props) => props.color || "blue"};
   padding: 1rem;
   display: flex;
+  width: 1024px;
+  margin: 0 auto;
+  @media (max-width: 1024px) {
+    width: 768px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+  }
 `;
 
 const Button = styled.button`
@@ -41,11 +49,17 @@ const Button = styled.button`
   }
 `;
 
+const MyInput = styled("input")`
+  background: gray;
+  margin-left: 10px;
+`;
+
 const StyledComponent = () => {
   return (
     <Box color="black">
       <Button>안녕하세요</Button>
       <Button inverted={true}>테두리만</Button>
+      <MyInput />
     </Box>
   );
 };
