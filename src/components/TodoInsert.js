3768d9f79076fe0d2a97916cbe617309b8b27513
unknown <kweyz@daum.net>
2022-02-07 15:23:06 +0900

    todoInsert input state

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,10 +1,18 @@
+import { useState, useCallback } from 'react';
 import { MdAddTask } from 'react-icons/md';
 import './TodoInsert.scss';
 
 const TodoInsert = () => {
+  const [value, setValue] = useState('');
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value);
+    console.log(e.target.value);
+  }, []);
+
   return (
     <form className="TodoInsert">
-      <input placeholder="Add Task"></input>
+      <input placeholder="Add Task" value={value} onChange={onChange}></input>
       <button type="submit">
         <MdAddTask />
       </button>
