4a45d2dda8cc557dd7f3fe979c3c345c8fb4b71c
unknown <kweyz@daum.net>
2022-02-09 15:23:34 +0900

    many data rerendering

--- src/App.js
+++ src/App.js
@@ -4,27 +4,23 @@ import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
-const App = () => {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      text: 'react basic',
-      checked: true,
-    },
-    {
-      id: 2,
-      text: 'component style',
-      checked: true,
-    },
-    {
-      id: 3,
-      text: 'todo app',
+function creatBulkTodos() {
+  const array = [];
+  for (let i = 1; i <= 2500; i++) {
+    array.push({
+      id: i,
+      text: `할 일 ${i}`,
       checked: false,
-    },
-  ]);
+    });
+  }
+  return array;
+}
+
+const App = () => {
+  const [todos, setTodos] = useState(creatBulkTodos);
 
   // ref를 사용하여 변수 담기
-  const nextId = useRef(4);
+  const nextId = useRef(2501);
 
   const onInsert = useCallback(
     (text) => {
