50119df3e2d92f9b4fd2a687a5c5d17500403781
unknown <kweyz@daum.net>
2022-02-12 15:28:51 +0900

    Query Parameter useSearchParams Hook

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,30 @@
-import { Link } from "react-router-dom"
+import { Link, useLocation, useSearchParams } from "react-router-dom";
 
 const About = () => {
-    return (
-        <div>
-            <h1>About</h1>
-            <p>리액트 라우터 기초를 십습해 보는 프로젝트 </p>
-            <Link to='/'>Home</Link>
-        </div>
-    )
-}
+  const [searchParams, setSearchParams] = useSearchParams();
+  const detail = searchParams.get("detail");
+  const mode = searchParams.get("mode");
 
-export default About
+  const onToggleDetail = () => {
+    setSearchParams({ mode, detail: detail === "true" ? false : true });
+  };
+
+  const onIncreaseMode = () => {
+    const nextMode = mode === null ? 1 : parseInt(mode) + 1;
+    setSearchParams({ mode: nextMode, detail });
+  };
+
+  return (
+    <div>
+      <h1>About</h1>
+      <p>리액트 라우터 기초를 십습해 보는 프로젝트 </p>
+      <p>detail: {detail}</p>
+      <p>mode: {mode}</p>
+      <button onClick={onToggleDetail}>Toggle detail</button>
+      <button onClick={onIncreaseMode}>mode + 1</button>
+      <Link to="/">Home</Link>
+    </div>
+  );
+};
+
+export default About;
