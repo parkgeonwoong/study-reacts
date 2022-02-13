3098b3299707a1a4906e7dd5bd9850a04c34f343
unknown <kweyz@daum.net>
2022-02-13 16:40:57 +0900

    Route: Index props

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ const App = () => {
   return (
     <div>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/profiles/:username" element={<Profile />} />
         </Route>
--- src/Layout.js
+++ src/Layout.js
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
     <div>
       <header style={{ background: "lightgray", padding: 16, fontSize: 24 }}>
         header
+        <Link
+          to="/"
+          style={{ textDecoration: "none", color: "black", padding: 15 }}
+        >
+          Home
+        </Link>
       </header>
       <main>
         <Outlet />
