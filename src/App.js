21e94b20e2d8a494b89c5eb28bc5f850e78e1a00
unknown <kweyz@daum.net>
2022-02-13 15:28:46 +0900

    Article init

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
-import Profile from './pages/Profile';
-
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Articles from "./pages/Articles";
+import Article from "./pages/Article";
 
 const App = () => {
   return (
     <div>
-      <Routes >
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/profiles/:username' element={<Profile />} />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+        <Route path="/articles" element={<Articles />} />
+        <Route path="/articles/:id" element={<Article />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
 export default App;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,29 @@
 import { Link } from "react-router-dom";
 
 const Home = () => {
-
-    return ( 
-        <div>
-            <h1>Home</h1>
-            <p>홈, 가장 먼저 보여주는 페이지 </p>
-            <ul>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to='/profiles/woongs'>Woongs Profile</Link>
-                </li>
-                <li>
-                    <Link to='/profiles/node'>Node Profile</Link>
-                </li>
-                <li>
-                    <Link to='/profiles/void'>Not Profile</Link>
-                </li>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>홈, 가장 먼저 보여주는 페이지 </p>
+      <ul>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/profiles/woongs">Woongs Profile</Link>
+        </li>
+        <li>
+          <Link to="/profiles/node">Node Profile</Link>
+        </li>
+        <li>
+          <Link to="/profiles/void">Not Profile</Link>
+        </li>
+        <li>
+          <Link to="/articles">게시글 목록</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default Home;
