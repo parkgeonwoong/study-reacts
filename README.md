84cb2125d6eec98ee8fd2e1362733f4f2f934495
unknown <kweyz@daum.net>
2022-02-13 16:30:53 +0900

    Outlet Hook: common Layout component

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # react-router tutorial
 
+- 리액트 라우터 튜토리얼을 진행해보고 개념을 정리
 - react-router V6.2
-- [react-router](https://reactrouter.com/docs/en/v6/getting-started/installation)
+- [react-router Document](https://reactrouter.com/docs/en/v6/getting-started/installation)
 
+<br>
+
+## 알아야 하는 개념
+
+- 라우팅이란?
+- SPA 란?
+- react-router 적용 사용법
+  - 프로젝트 생성 및 라이브러리 설치
+  - 라우터 적용
+  - 페이지 컴포넌트 만들기
+  - Route 컴포넌트로 경로에 원하는 컴포넌트 보이기
+  - Link 컴포넌트 사용 이동
+- URL 파라미터
+- 쿼리 스트링
+- 중첩된 라우트
+- 부가기능
--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import About from "./pages/About";
 import Profile from "./pages/Profile";
 import Articles from "./pages/Articles";
 import Article from "./pages/Article";
+import Layout from "./Layout";
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/profiles/:username" element={<Profile />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profiles/:username" element={<Profile />} />
+        </Route>
         <Route path="/articles" element={<Articles />}>
           <Route path=":id" element={<Article />} />
         </Route>
