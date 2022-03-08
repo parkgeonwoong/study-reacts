a790115015adec88ac6cd764dddfdfc79a30a1cd
unknown <kweyz@daum.net>
2022-03-08 16:43:18 +0900

    NewsItem

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -31,6 +31,27 @@ const NewsItemBlock = styled.div`
   }
 `;
 
-const NewsItem = () => {};
+const NewsItem = ({ article }) => {
+  const { title, description, url, urlToImage } = article;
+  return (
+    <NewsItemBlock>
+      {urlToImage && (
+        <div className="thumbnail">
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            <img src={urlToImage} alt="thumbnail"></img>
+          </a>
+        </div>
+      )}
+      <div className="contents">
+        <h2>
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {title}
+          </a>
+        </h2>
+        <p>{description}</p>
+      </div>
+    </NewsItemBlock>
+  );
+};
 
 export default NewsItem;
