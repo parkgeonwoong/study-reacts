5ca72ff3c5c66027086f822b8cbf88df8f58d79a
unknown <kweyz@daum.net>
2022-03-08 16:51:07 +0900

    News UI newsList

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import "./App.css";
 import axios from "axios";
 import { useState } from "react";
+import NewsList from "./components/NewsList";
 
-const App = () => {};
+const App = () => {
+  return <NewsList />;
+};
 
 export default App;
