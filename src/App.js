f9f310e4e67df116a59aaa0b65acf902a01c64dc
unknown <kweyz@daum.net>
2022-03-08 16:39:16 +0900

    newsApi

--- src/App.js
+++ src/App.js
@@ -2,33 +2,6 @@ import "./App.css";
 import axios from "axios";
 import { useState } from "react";
 
-const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        "https://jsonplaceholder.typicode.com/todos/1"
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>Load</button>
-      </div>
-      {data && (
-        <textarea
-          row={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
-};
+const App = () => {};
 
 export default App;
---
+++ src/AppApi.js
@@ -0,0 +1,34 @@
+import "./App.css";
+import axios from "axios";
+import { useState } from "react";
+
+const App = () => {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=kr&category=technology&apiKey=c4168f2b05c54d37809d5a331d288ea4"
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={onClick}>Load</button>
+      </div>
+      {data && (
+        <textarea
+          row={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
+    </div>
+  );
+};
+
+export default App;
