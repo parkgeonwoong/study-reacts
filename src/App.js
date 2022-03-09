fb8a7af92385fc616ad3a31add66847a4fe6ff40
unknown <kweyz@daum.net>
2022-03-09 20:48:51 +0900

    category UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
-import axios from "axios";
-import { useState } from "react";
+import { useCallback, useState } from "react";
 import NewsList from "./components/NewsList";
+import Categories from "./components/Categories";
 
 const App = () => {
-  return <NewsList />;
+  const [category, setCategory] = useState("all");
+  const onSelect = useCallback((category) => setCategory(category), []);
+
+  return (
+    <div>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </div>
+  );
 };
 
 export default App;
