aa6f703b3ed0a9515a28496b0200b73b6b6eb94c
unknown <kweyz@daum.net>
2022-03-10 21:36:35 +0900

    Use react-router V6

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import "./App.css";
-import { useCallback, useState } from "react";
-import NewsList from "./components/NewsList";
-import Categories from "./components/Categories";
+import { Routes, Route } from "react-router-dom";
+import NewsPage from "./pages/NewsPage";
 
 const App = () => {
-  const [category, setCategory] = useState("all");
-  const onSelect = useCallback((category) => setCategory(category), []);
-
   return (
-    <div>
-      <Categories category={category} onSelect={onSelect} />
-      <NewsList category={category} />
-    </div>
+    <Routes>
+      <Route path="/" element={<NewsPage />} />
+      <Route path="/:category" element={<NewsPage />} />
+    </Routes>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
 import AppApi from "./AppApi";
 
 ReactDOM.render(
-  <React.StrictMode>
-    {/* <AppApi /> */}
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <React.StrictMode>
+      {/* <AppApi /> */}
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
