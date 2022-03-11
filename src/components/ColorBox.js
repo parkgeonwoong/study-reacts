85ad89cc1715945d0e0995ba1f295a2501f6559b
unknown <kweyz@daum.net>
2022-03-11 16:58:27 +0900

    SelectColor consumer

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -4,9 +4,10 @@ const ColorBox = () => {
   return (
     <ColorConsumer>
       {(value) => (
-        <div>
+        <div style={{ display: "flex" }}>
           <div
             style={{
+              margin: "0 10px",
               width: "64px",
               height: "64px",
               background: value.state.color,
