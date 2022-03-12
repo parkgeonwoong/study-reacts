5669a5b60b35862ab98ec17c431413333cbbd836
unknown <kweyz@daum.net>
2022-03-12 17:25:09 +0900

    redux-store

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
+import { createStore } from "redux";
+
 const divToggle = document.querySelector(".toggle");
 const counter = document.querySelector("h1");
 const btnIncrease = document.querySelector("#increase");
 const btnDecrease = document.querySelector("#decrease");
 
-// 액션 생성
+// 1.액션 생성
 const TOGGLE_SWITCH = "TOGGLE_SWITCH";
 const INCREASE = "INCREASE";
 const DECREASE = "DECREASE";
 
-// 액션 객체
+// 2.액션 객체
 const toggleSwitch = () => ({ type: TOGGLE_SWITCH });
 const increase = () => ({ type: INCREASE, difference });
 const decrease = () => ({ type: DECREASE });
 
-// 초깃값 설정
+// 3.초깃값 설정
 const initialState = {
   toggle: false,
   counter: 0,
 };
 
-// 리듀서 함수 정의
+// 4.리듀서 함수 정의
 function reducer(state = initialState, action) {
   switch (action.type) {
     case TOGGLE_SWITCH:
@@ -41,3 +43,22 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+// 5.스토어 만들기
+const store = createStore(reducer);
+
+// 6.상태가 업데이트될 때마다 호출하는 함수
+const render = () => {
+  const state = store.getState(); // 현재 상태 불러오기
+  // 토글 처리
+  if (state.toggle) {
+    divToggle.classList.add("active");
+  } else {
+    divToggle.classList.remove("active");
+  }
+
+  // 카운터 처리
+  counter.innerText = state.counter;
+};
+
+render();
