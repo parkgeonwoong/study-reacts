8ed12911654d80f3926f32095ac521a1e8b02189
unknown <kweyz@daum.net>
2022-03-13 21:53:22 +0900

    develop store, Provider

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -3,8 +3,8 @@ const Counter = ({ number, onIncrease, onDecrease }) => {
     <div>
       <h1>{number}</h1>
       <div>
-        <button onCLick={onIncrease}>+1</button>
-        <button onCLick={onDecrease}>-1</button>
+        <button onClick={onIncrease}>+1</button>
+        <button onClick={onDecrease}>-1</button>
       </div>
     </div>
   );
