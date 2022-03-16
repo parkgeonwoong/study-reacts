c22b0127fd3b350359c0f23dfafb69619529484a
unknown <kweyz@daum.net>
2022-03-16 17:21:52 +0900

    redux-actions: createAction, handleActions

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,10 +1,12 @@
+import { createAction, handleActions } from "redux-actions";
+
 // 액션 타입 정의
 const INCREASE = "counter/INCREASE";
 const DECREASE = "counter/DECREASE";
 
 // 액션 생성 함수
-export const increase = () => ({ type: INCREASE });
-export const decrease = () => ({ type: DECREASE });
+export const increase = () => createAction(INCREASE);
+export const decrease = () => createAction(DECREASE);
 
 // 초기값 설정
 const initalState = {
@@ -12,19 +14,12 @@ const initalState = {
 };
 
 // 리듀서 함수
-function counter(state = initalState, action) {
-  switch (action.type) {
-    case INCREASE:
-      return {
-        number: state.number + 1,
-      };
-    case DECREASE:
-      return {
-        number: state.number - 1,
-      };
-    default:
-      return state;
-  }
-}
+const counter = handleActions(
+  {
+    [INCREASE]: (state, action) => ({ number: state.number + 1 }),
+    [DECREASE]: (state, action) => ({ number: state.number - 1 }),
+  },
+  initalState
+);
 
 export default counter;
