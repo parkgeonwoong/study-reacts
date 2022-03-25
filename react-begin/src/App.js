7a7ffbcc5c1c4beba42a4c6b07a26a21dfa39f7c
unknown <kweyz@daum.net>
2022-03-25 17:08:38 +0900

    API fetch

--- react-begin/src/App.js
+++ react-begin/src/App.js
@@ -1,7 +1,20 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  return <div></div>;
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=100");
+  }, []);
+
+  return (
+    <div>
+      <h1>This Coins</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <ul></ul>
+    </div>
+  );
 }
 
 export default App;
