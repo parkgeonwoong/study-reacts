75cf8a792e00e43429cd61fd2ab095d494936800
unknown <kweyz@daum.net>
2022-03-25 16:46:06 +0900

    Todo

--- react-begin/src/App.js
+++ react-begin/src/App.js
@@ -1,40 +1,7 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [todo, setTodo] = useState("");
-  const [todos, setTodos] = useState([]);
-
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    // console.log(todo);
-    if (todo === "") {
-      return;
-    }
-    setTodos((currentArray) => [todo, ...currentArray]);
-    // setTodos(() => todos.concat(todo));
-    setTodo("");
-  };
-  console.log(todos);
-
-  return (
-    <div>
-      <h1>My ToDo ({todos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={todo}
-          type="text"
-          placeholder="Todo"
-        />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      {todos.map((item, index) => (
-        <li key={index}>{item}</li>
-      ))}
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
