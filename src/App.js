335f8d562c86284bf368e01f21d7e792acc70e05
unknown <kweyz@daum.net>
2022-08-08 23:01:21 +0900

    state, event 적용

--- src/App.js
+++ src/App.js
@@ -1,14 +1,46 @@
+/* eslint-disable */
+
 import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  let post = "First Name server dummy data";
+  const [post, setPost] = useState([
+    "남자 코트 추천",
+    "맛집 추천",
+    "카페 추천",
+  ]);
+  const [like, setLike] = useState(0);
+
+  const handleCount = () => {
+    setLike((num) => num + 1);
+  };
+
+  const handleGender = () => {
+    console.log(post.map((item) => item));
+  };
+
+  const items = post.map((item, index) => (
+    <div className="list" key={index}>
+      <h4>
+        {item}
+        <span onClick={handleCount}> ✨</span> {like}
+      </h4>
+      <p>2월 17일 발행</p>
+    </div>
+  ));
 
   return (
     <div className="App">
       <div className="black-nav">
-        <h2 style={{ color: "red", fontSize: "20px" }}>Blog</h2>
+        <h2 style={{ color: "white" }}>ReactBlog</h2>
       </div>
-      <h3>{post}</h3>
+      <div className="list">
+        <h4 onClick={handleGender}>
+          <span onClick={handleCount}>{post[0]} ✨</span> {like}
+        </h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <div>{items}</div>
     </div>
   );
 }
