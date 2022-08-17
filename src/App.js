c40df89ef509f00415d234e9d5c8ecdf480439b4
parkgeonwoong <hk77kh@gmail.com>
2022-08-17 12:57:57 +0900

    Feat: BootStrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <div></div>
+      <Navbar bg="light" variant="light">
+        <Container>
+          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
   );
 }
