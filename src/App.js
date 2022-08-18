4e9c0c09e42e1a4376e442f4f5137a134df57d52
unknown <kweyz@daum.net>
2022-08-18 22:43:51 +0900

    image 실험

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
@@ -14,6 +16,37 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+
+      <div className="main-bg"></div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img
+              src="https://images.unsplash.com/photo-1492433483212-7b3e7e53e6d9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://images.unsplash.com/photo-1611486212557-88be5ff6f941?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://images.unsplash.com/photo-1561677978-583a8c7a4b43?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+              width="80%"
+            />
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
