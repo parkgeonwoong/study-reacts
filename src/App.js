fd2eb45f821e8d32b9184ddf5842aacdb286bbff
unknown <kweyz@daum.net>
2022-08-21 15:32:32 +0900

    Feat: 리액트라우터 적용

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Navbar, Container, Nav } from "react-bootstrap";
-import data from "./data";
 import { useState } from "react";
+import data from "./data";
 import Item from "./components/Item";
 import url from "./assets/UrlSrc";
+import { Routes, Route, Link } from "react-router-dom";
+import Detail from "./components/Detail";
 
 function App() {
   const [product, setProduct] = useState(data);
@@ -17,26 +19,43 @@ function App() {
       {/* Header */}
       <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Link to="/" className="home">
+            WoongShop
+          </Link>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Detail</Nav.Link>
             <Nav.Link href="#features">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
 
       {/* Main */}
-      <div className="main-bg"></div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <div className="main-bg"></div>
 
-      <div className="container">
-        <div className="row">
-          {product.map((item, index) => {
-            return (
-              <Item key={index} product={item} url={urlSrc} index={index} />
-            );
-          })}
-        </div>
-      </div>
+              <div className="container">
+                <div className="row">
+                  {product.map((item, index) => {
+                    return (
+                      <Item
+                        key={index}
+                        product={item}
+                        url={urlSrc}
+                        index={index}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            </>
+          }
+        />
+        <Route path="/detail" element={<Detail />} />
+      </Routes>
     </div>
   );
 }
