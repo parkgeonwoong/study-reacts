e924c3aabf6a8cc77939c592d099e0c84543b449
parkgeonwoong <hk77kh@gmail.com>
2022-11-29 15:42:40 +0900

    Feat : styled 값 변경, 확장 해보기

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
+/**
+ * @desc : styled-components
+ */
+import styled from "styled-components";
+
 function App() {
-  return <div></div>;
+  return (
+    <DivFater>
+      <DivBox bgColor="teal" />
+      <DivBox bgColor="tomato" />
+      <BoxCircle bgColor="blue" />
+    </DivFater>
+  );
 }
+
+// 스타일
+const DivFater = styled.div`
+  display: flex;
+`;
+
+// 변수 사용해보기
+const DivBox = styled.div`
+  background-color: ${(props) => props.bgColor};
+  width: 100px;
+  height: 100px;
+`;
+
+// 확장
+const BoxCircle = styled(DivBox)`
+  border-radius: 50%;
+`;
 
 export default App;
