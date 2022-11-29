63b1975107e3b2f91066eccd74b56aee13335e53
parkgeonwoong <hk77kh@gmail.com>
2022-11-29 16:23:45 +0900

    Feat : 태그만 바꿔보기, 속성값 넣기

--- src/App.js
+++ src/App.js
@@ -7,27 +7,37 @@ function App() {
   return (
     <DivFater>
       <DivBox bgColor="teal" />
-      <DivBox bgColor="tomato" />
+      {/* 4. 태그만 바꾸는 방법 */}
+      <DivBox as="button" bgColor="tomato">
+        버튼?
+      </DivBox>
       <BoxCircle bgColor="blue" />
+      <Input />
+      <Input />
     </DivFater>
   );
 }
 
-// 스타일
+// 1. 스타일-컴포넌트
 const DivFater = styled.div`
   display: flex;
 `;
 
-// 변수 사용해보기
+// 2. 변수 사용해보기
 const DivBox = styled.div`
   background-color: ${(props) => props.bgColor};
   width: 100px;
   height: 100px;
 `;
 
-// 확장
+// 3. 확장
 const BoxCircle = styled(DivBox)`
   border-radius: 50%;
+`;
+
+// 5. 속성 대입하기
+const Input = styled.input.attrs({ required: true })`
+  background-color: skyblue;
 `;
 
 export default App;
