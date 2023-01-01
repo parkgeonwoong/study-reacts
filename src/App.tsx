c43701c8644c6efdea53c4332cd383916d90f770
parkgeonwoong <hk77kh@gmail.com>
2023-01-01 15:55:14 +0900

    Feat : input과 atom, selector 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import { HelmetProvider } from "react-helmet-async";
+import React from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
 
 function App() {
-  return <div></div>;
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const hours = useRecoilValue(hourSelector);
+  const onMinuteChange = (event: React.FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value);
+  };
+
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinuteChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input value={hours} type="number" placeholder="Hours" readOnly />
+    </div>
+  );
 }
 
 export default App;
